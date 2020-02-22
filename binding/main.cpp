8291d419c16e12f43f8a5bd9da310cf064c5d7cb
Kasumi Hanazuki <kasumi@rollingapple.net>
2020-02-22 16:58:58 +0000

    Export Jsonnet as a class

--- binding/main.cpp
+++ binding/main.cpp
@@ -3,16 +3,36 @@ extern "C" {
 #include <libjsonnet.h>
 }
 
-Napi::String Method(const Napi::CallbackInfo& info) {
-  Napi::Env env = info.Env();
+class Jsonnet: public Napi::ObjectWrap<Jsonnet> {
+public:
+  static Napi::Object Init(Napi::Env env, Napi::Object exports);
+  Jsonnet(const Napi::CallbackInfo &info);
 
+private:
+  static Napi::FunctionReference constructor;
+
+  static Napi::Value GetVersion(const Napi::CallbackInfo &info);
+};
+
+Napi::FunctionReference Jsonnet::constructor;
+
+Napi::Object Jsonnet::Init(Napi::Env env, Napi::Object exports) {
+  auto func = DefineClass(env, "Jsonnet", {
+      StaticAccessor("version", &Jsonnet::GetVersion, nullptr),
+    });
+
+  constructor = Napi::Persistent(func);
+  constructor.SuppressDestruct();
+  return func;
+}
+
+Napi::Value Jsonnet::GetVersion(const Napi::CallbackInfo& info) {
+  auto env = info.Env();
   return Napi::String::New(env, ::jsonnet_version());
 }
 
-Napi::Object Init(Napi::Env env, Napi::Object exports) {
-  exports.Set(Napi::String::New(env, "hello"),
-              Napi::Function::New(env, Method));
-  return exports;
+static Napi::Object init_node_jsonnet(Napi::Env env, Napi::Object exports) {
+  return Jsonnet::Init(env, exports);
 }
 
-NODE_API_MODULE(hello, Init)
+NODE_API_MODULE(jsonnet, init_node_jsonnet)
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
-var addon = require('bindings')('node-jsonnet');
+type Jsonnet = {
+  version: string;
 
-console.log(addon.hello()); // 'world'
+  /* new(): {
+   *   extVar(key: string, value: string): void;
+   *   extCode(key: string, value: string): void;
+   *   tlaVar(key: string, value: string): void;
+   *   tlaCode(key: string, value: string): void;
+
+   *   evaluateFile(filename: string): string;
+   *   evaluateSnippet(snippet: string, filename: string | null): string;
+   * }; */
+}
+
+const Jsonnet: Jsonnet = require('bindings')('node-jsonnet');
+
+console.log(Jsonnet.version);
