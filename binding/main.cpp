8ab4039e4968d7b4f4eb23dcd47a0a0185cd9240
Kasumi Hanazuki <kasumi@rollingapple.net>
2020-02-23 16:27:02 +0000

    -Wextra

--- binding/main.cpp
+++ binding/main.cpp
@@ -1,6 +1,6 @@
 #include "Jsonnet.hpp"
 
-static Napi::Object init_node_jsonnet(Napi::Env env, Napi::Object _exports) {
+static Napi::Object init_node_jsonnet(Napi::Env env, Napi::Object) {
   return nodejsonnet::Jsonnet::init(env);
 }
 NODE_API_MODULE(jsonnet, init_node_jsonnet)
