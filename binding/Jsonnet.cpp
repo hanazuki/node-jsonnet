1dfed29a16609af61d7fdc5365a952380f254366
Kasumi Hanazuki <kasumi@rollingapple.net>
2020-02-23 16:19:21 +0000

    More auto consts

--- binding/Jsonnet.cpp
+++ binding/Jsonnet.cpp
@@ -32,18 +32,18 @@ namespace nodejsonnet {
 
   Napi::Value Jsonnet::evaluateFile(const Napi::CallbackInfo& info) {
     auto const env = info.Env();
-    std::string const filename = info[0].As<Napi::String>();
+    auto const filename = info[0].As<Napi::String>();
 
-    auto worker = new JsonnetWorker(env, vm, std::make_unique<JsonnetWorker::EvaluateFileOp>(filename));
-    auto promise = worker->Promise();
+    auto const worker = new JsonnetWorker(env, vm, std::make_unique<JsonnetWorker::EvaluateFileOp>(filename));
+    auto const promise = worker->Promise();
     worker->Queue();
     return promise;
   }
 
   Napi::Value Jsonnet::evaluateSnippet(const Napi::CallbackInfo& info) {
     auto const env = info.Env();
-    std::string const snippet = info[0].As<Napi::String>();
-    std::string const filename = info.Length() < 2 ? "(snippet)" : info[1].As<Napi::String>().Utf8Value();
+    auto const snippet = info[0].As<Napi::String>();
+    auto const filename = info.Length() < 2 ? "(snippet)" : info[1].As<Napi::String>().Utf8Value();
 
     auto worker = new JsonnetWorker(env, vm, std::make_unique<JsonnetWorker::EvaluateSnippetOp>(snippet, filename));
     auto promise = worker->Promise();
@@ -53,49 +53,49 @@ namespace nodejsonnet {
 
   Napi::Value Jsonnet::extString(const Napi::CallbackInfo& info) {
     auto const env = info.Env();
-    std::string const key = info[0].As<Napi::String>();
-    std::string const val = info[1].As<Napi::String>();
+    auto const key = info[0].As<Napi::String>();
+    auto const val = info[1].As<Napi::String>();
 
-    vm.extVar(key.c_str(), val.c_str());
+    vm.extVar(key, val);
 
     return env.Undefined();
   }
 
   Napi::Value Jsonnet::extCode(const Napi::CallbackInfo& info) {
     auto const env = info.Env();
-    std::string const key = info[0].As<Napi::String>();
-    std::string const val = info[1].As<Napi::String>();
+    auto const key = info[0].As<Napi::String>();
+    auto const val = info[1].As<Napi::String>();
 
-    vm.extCode(key.c_str(), val.c_str());
+    vm.extCode(key, val);
 
     return env.Undefined();
   }
 
   Napi::Value Jsonnet::tlaString(const Napi::CallbackInfo& info) {
     auto const env = info.Env();
-    std::string const key = info[0].As<Napi::String>();
-    std::string const val = info[1].As<Napi::String>();
+    auto const key = info[0].As<Napi::String>();
+    auto const val = info[1].As<Napi::String>();
 
-    vm.tlaVar(key.c_str(), val.c_str());
+    vm.tlaVar(key, val);
 
     return env.Undefined();
   }
 
   Napi::Value Jsonnet::tlaCode(const Napi::CallbackInfo& info) {
     auto const env = info.Env();
-    std::string const key = info[0].As<Napi::String>();
-    std::string const val = info[1].As<Napi::String>();
+    auto const key = info[0].As<Napi::String>();
+    auto const val = info[1].As<Napi::String>();
 
-    vm.tlaCode(key.c_str(), val.c_str());
+    vm.tlaCode(key, val);
 
     return env.Undefined();
   }
 
   Napi::Value Jsonnet::addJpath(const Napi::CallbackInfo& info) {
     auto const env = info.Env();
-    std::string const path = info[0].As<Napi::String>();
+    auto const path = info[0].As<Napi::String>();
 
-    vm.jpathAdd(path.c_str());
+    vm.jpathAdd(path);
 
     return env.Undefined();
   }
