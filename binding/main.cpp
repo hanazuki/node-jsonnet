c91192e9be1b9831b11828fc341747c26705f806
Kasumi Hanazuki <kasumi@rollingapple.net>
2020-02-23 08:43:06 +0000

    binding: contructor

--- binding/main.cpp
+++ binding/main.cpp
@@ -1,3 +1,4 @@
+#include <memory>
 #include <napi.h>
 extern "C" {
 #include <libjsonnet.h>
@@ -8,13 +9,18 @@ public:
   static Napi::Object init(Napi::Env env);
 
   explicit Jsonnet(const Napi::CallbackInfo &info);
-  ~Jsonnet();
 
 private:
   static Napi::FunctionReference constructor;
   static Napi::Value getVersion(const Napi::CallbackInfo &info);
 
-  JsonnetVm *vm;
+  std::shared_ptr<JsonnetVm> vm;
+  Napi::Value evaluateFile(const Napi::CallbackInfo &info);
+  Napi::Value evaluateSnippet(const Napi::CallbackInfo &info);
+
+  std::shared_ptr<char> vmAcquire(char *buf) {
+    return {buf, [vm](char *buf){ jsonnet_realloc(vm.get(), buf, 0); }};
+  }
 };
 
 Napi::FunctionReference Jsonnet::constructor;
@@ -30,16 +36,25 @@ Napi::Object Jsonnet::init(Napi::Env env) {
 }
 
 Jsonnet::Jsonnet(const Napi::CallbackInfo &info):
-  Napi::ObjectWrap<Jsonnet>(info), vm(jsonnet_make()) {
-}
-
-Jsonnet::~Jsonnet() {
-  jsonnet_destroy(vm);
+  Napi::ObjectWrap<Jsonnet>(info), vm(jsonnet_make(), jsonnet_destroy) {
 }
 
 Napi::Value Jsonnet::getVersion(const Napi::CallbackInfo& info) {
   auto env = info.Env();
   return Napi::String::New(env, ::jsonnet_version());
+}
+
+Napi::Value Jsonnet::evaluateFile(const Napi::CallbackInfo& info) {
+  auto env = info.Env();
+
+  std::string const filename = info[0].As<Napi::String>();
+  int error;
+  auto result = jsonnet_evaluate_file(vm.get(), filename.c_str(), &error);
+  if(error != 0) {
+    
+  }
+
+  return Napi::String::New(env, result);
 }
 
 static Napi::Object init_node_jsonnet(Napi::Env env, Napi::Object _exports) {
