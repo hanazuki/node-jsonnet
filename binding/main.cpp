67432fd23e02a4f66bd6287ea63f9651a8317428
Kasumi Hanazuki <kasumi@rollingapple.net>
2020-02-23 09:25:11 +0000

    Implement evaluateFile/evaludateSnippet

--- binding/main.cpp
+++ binding/main.cpp
@@ -6,7 +6,7 @@ extern "C" {
 
 class Jsonnet: public Napi::ObjectWrap<Jsonnet> {
 public:
-  static Napi::Object init(Napi::Env env);
+  static Napi::Object init(const Napi::Env env);
 
   explicit Jsonnet(const Napi::CallbackInfo &info);
 
@@ -18,16 +18,18 @@ private:
   Napi::Value evaluateFile(const Napi::CallbackInfo &info);
   Napi::Value evaluateSnippet(const Napi::CallbackInfo &info);
 
-  std::shared_ptr<char> vmAcquire(char *buf) {
-    return {buf, [vm](char *buf){ jsonnet_realloc(vm.get(), buf, 0); }};
+  std::shared_ptr<char> borrowBuffer(char *buf) {
+    return {buf, [vm = vm](char *buf){ jsonnet_realloc(vm.get(), buf, 0); }};
   }
 };
 
 Napi::FunctionReference Jsonnet::constructor;
 
 Napi::Object Jsonnet::init(Napi::Env env) {
-  auto func = DefineClass(env, "Jsonnet", {
+  auto const func = DefineClass(env, "Jsonnet", {
       StaticAccessor("version", &Jsonnet::getVersion, nullptr),
+      InstanceMethod("evaluateFile", &Jsonnet::evaluateSnippet),
+      InstanceMethod("evaluateSnippet", &Jsonnet::evaluateSnippet),
     });
 
   constructor = Napi::Persistent(func);
@@ -36,29 +38,42 @@ Napi::Object Jsonnet::init(Napi::Env env) {
 }
 
 Jsonnet::Jsonnet(const Napi::CallbackInfo &info):
-  Napi::ObjectWrap<Jsonnet>(info), vm(jsonnet_make(), jsonnet_destroy) {
+  Napi::ObjectWrap<Jsonnet>(info), vm{jsonnet_make(), jsonnet_destroy} {
 }
 
 Napi::Value Jsonnet::getVersion(const Napi::CallbackInfo& info) {
-  auto env = info.Env();
+  auto const env = info.Env();
   return Napi::String::New(env, ::jsonnet_version());
 }
 
 Napi::Value Jsonnet::evaluateFile(const Napi::CallbackInfo& info) {
-  auto env = info.Env();
-
+  auto const env = info.Env();
   std::string const filename = info[0].As<Napi::String>();
+
   int error;
-  auto result = jsonnet_evaluate_file(vm.get(), filename.c_str(), &error);
+  auto result = borrowBuffer(jsonnet_evaluate_file(vm.get(), filename.c_str(), &error));
   if(error != 0) {
-    
+    throw Napi::Error::New(env, result.get());
   }
 
-  return Napi::String::New(env, result);
+  return Napi::String::New(env, result.get());
+}
+
+Napi::Value Jsonnet::evaluateSnippet(const Napi::CallbackInfo& info) {
+  auto const env = info.Env();
+  std::string const snippet = info[0].As<Napi::String>();
+  std::string const filename = info.Length() < 2 ? "(snippet)" : static_cast<std::string>(info[1].As<Napi::String>());
+
+  int error;
+  auto const result = borrowBuffer(jsonnet_evaluate_snippet(vm.get(), filename.c_str(), snippet.c_str(), &error));
+  if(error != 0) {
+    throw Napi::Error::New(env, result.get());
+  }
+
+  return Napi::String::New(env, result.get());
 }
 
 static Napi::Object init_node_jsonnet(Napi::Env env, Napi::Object _exports) {
   return Jsonnet::init(env);
 }
-
 NODE_API_MODULE(jsonnet, init_node_jsonnet)
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,22 @@
 type Jsonnet = {
   version: string;
 
-  /* new(): {
-   *   extVar(key: string, value: string): void;
-   *   extCode(key: string, value: string): void;
-   *   tlaVar(key: string, value: string): void;
-   *   tlaCode(key: string, value: string): void;
-
-   *   evaluateFile(filename: string): string;
-   *   evaluateSnippet(snippet: string, filename: string | null): string;
-   * }; */
+  new(): {
+    /* extVar(key: string, value: string): void;
+     * extCode(key: string, value: string): void;
+     * tlaVar(key: string, value: string): void;
+     * tlaCode(key: string, value: string): void;
+     */
+    evaluateFile(filename: string): string;
+    evaluateSnippet(snippet: string): string;
+    evaluateSnippet(snippet: string, filename: string): string;
+  };
 }
 
 const Jsonnet: Jsonnet = require('bindings')('node-jsonnet');
 
 console.log(Jsonnet.version);
+
+const jsonnet = new Jsonnet();
+
+console.log(jsonnet.evaluateSnippet("{a: 1 + 1}"));
