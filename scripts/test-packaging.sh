cb242f06bf3f4c22895ff86e619e6405708b7b05
Kasumi Hanazuki <kasumi@rollingapple.net>
2020-05-12 14:01:07 +0000

    Code coverage

--- scripts/test-packaging.sh
+++ scripts/test-packaging.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -eux
 
+__dir="$(cd "$(dirname "$BASH_SOURCE")" && pwd)"
+
+mkdir -p coverage
+TRACEFILE="$PWD/coverage/lcov.info"
+
 npm build
 rm -f *.tgz
 npm pack
@@ -8,5 +13,15 @@ tmpdir=$(mktemp -d)
 tar xf hanazuki-node-jsonnet-*.tgz -C "$tmpdir"
 pushd "$tmpdir/package"
 npm install-test
+if [[ ${NODE_JSONNET_ENABLE_COVERAGE-} ]]; then
+  mkdir -p coverage
+  lcov_opts=()
+  if [[ $NODE_JSONNET_ENABLE_COVERAGE = clang ]]; then
+    lcov_opts+=(--gcov-tool "${__dir}"/llvm-gcov.sh)
+  fi
+  lcov -c -d build/CMakeFiles -b "$(pwd -P)/src" --no-external -o coverage/lcov.info -t "$NODE_JSONNET_ENABLE_COVERAGE" ${lcov_opts[@]+"${lcov_opts[@]}"}
+  sed -i.bak -e "s|^SF:$(pwd -P)/|SF:|" coverage/lcov.info
+  lcov -a coverage/lcov.info -o "$TRACEFILE"
+fi
 popd
 rm -rf "$tmpdir"
