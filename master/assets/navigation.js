b032c1fd9afcb3fa5c4e7988d863a098305cca78
automate <automate@hanazuki.dev>
2023-09-12 05:07:31 +0000

    Generate docs
    Source: 7ffd869721e775b28bc647a6453bf686ce2a38ed

---
+++ master/assets/navigation.js
@@ -0,0 +1,1 @@
+window.navigationData = "data:application/octet-stream;base64,H4sIAAAAAAAAA4uuVipJrShRslLyKs7Py0stUdJRKkgsyQAKJOckFhenFutDJfQySnJzgLLZmXkpSlaGRha1Ouh6XYuK8otwGgCWJcKUgKLM3MySzLLUsMSc0lSEcSWVBQjDUBWhmmpkYGluaGpUGwsAX5mwTt0AAAA="
